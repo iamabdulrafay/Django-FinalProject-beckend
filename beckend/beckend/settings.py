814caa002c3a502c5ae19ed2313049bf17a9ab8d
iamabdulrafay <malik.khan2000@gmail.com>
2024-09-20 19:44:43 -0700

    aa

--- beckend/beckend/settings.py
+++ beckend/beckend/settings.py
@@ -32,7 +32,13 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 
+STATIC_URL="./static"
 
+STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles")
+
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,"static")
+]
 # # Application definition
 
 INSTALLED_APPS = [
