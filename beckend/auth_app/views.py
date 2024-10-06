ac940e6e2230b7b5582faea742abd0dd164dae6e
iamabdulrafay <malik.khan2000@gmail.com>
2024-10-06 16:58:56 -0700

    sss

--- beckend/auth_app/views.py
+++ beckend/auth_app/views.py
@@ -1,30 +1,47 @@
+from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import SignupSerializers, LoginSerializers
+from .serializers import SignupSerializers,LoginSerializers
 from rest_framework.views import APIView
 from django.contrib.auth import login
+
+from django.middleware.csrf import get_token
 from rest_framework.permissions import AllowAny
 
-class SignupSerializerViewSets(APIView):
-    permission_classes = [AllowAny]
 
-    def post(self, request):
-        serializers = SignupSerializers(data=request.data)
+from rest_framework.authtoken.models import Token
+
+class SignupSerializerViewSets(APIView):
+    permission_classes = [AllowAny]  
+
+    def post(self,request):
+        serializers=SignupSerializers(data=request.data)
+
         if serializers.is_valid():
             serializers.save()
-            return Response({"msg": "User created successfully!"}, status=status.HTTP_201_CREATED)
+            return Response({"msg:user create ssuccessfully!"},status=status.HTTP_201_CREATED)
         
-        print(serializers.errors)  # Debugging line
-        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializers.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+
+
 
 class LoginSerializerViewSets(APIView):
-    permission_classes = [AllowAny]
-
+    permission_classes = [AllowAny]  
     def post(self, request):
         serializer = LoginSerializers(data=request.data)
         if serializer.is_valid():
             user = serializer.validated_data['user']
-            login(request, user)
-            return Response({"msg": "Login successful"}, status=status.HTTP_200_OK)
+            login(request, user) 
+            
+            token, created = Token.objects.get_or_create(user=user)
+
+            return Response({
+                "msg": "Login successful",
+                "token": token.key,  # Return the token
+                "CSRFToken": get_token(request)
+            }, status=status.HTTP_200_OK)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
